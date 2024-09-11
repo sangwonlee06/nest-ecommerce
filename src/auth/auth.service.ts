ec42d9028ae2fc7c61491b1535b01c614f8cfb9e
sangwonlee06 <sangwonlee0622@outlook.com>
2024-09-11 16:25:28 -0700

    Add sign in

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import { CreateUserDto } from '../user/dto/create-user.dto';
+import { SignInUserDto } from '../user/dto/signin-user.dto';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
@@ -18,5 +20,26 @@ export class AuthService {
     const newUser = await this.userService.createUser(createUserDto);
     newUser.password = undefined;
     return newUser;
+  }
+
+  async signIn(signInUserDto: SignInUserDto) {
+    const user = await this.userService.getUserByEmail(signInUserDto.email);
+
+    // getUserByEmail should already throw an exception if the user is not found
+    // No need to handle a 'user not found' error here
+
+    const isPasswordCorrect = await bcrypt.compare(
+      signInUserDto.password,
+      user.password,
+    );
+
+    if (!isPasswordCorrect) {
+      throw new HttpException(
+        'Invalid email or password',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    return user;
   }
 }
