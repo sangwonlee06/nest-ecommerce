f1423e9613f07c11792639250cc144e879c827d5
sangwonlee06 <sangwonlee0622@outlook.com>
2024-09-16 14:21:31 -0700

    Add Swagger decorators for auth and product endpoints

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -14,7 +14,8 @@ import { RequestWithUserInterface } from './interfaces/requestWithUser.interface
 import { AccessTokenGuard } from './guards/access-token.guard';
 import { EmailVerificationDto } from '../user/dto/email-verification.dto';
 import { GoogleAuthGuard } from './guards/google-auth.guard';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { SignInUserDto } from '../user/dto/signin-user.dto';
 
 @ApiTags('auth')
 @Controller('auth')
@@ -22,22 +23,48 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('/signup')
+  @ApiBody({
+    type: CreateUserDto,
+    description: 'Data required to create a new user account.',
+  })
+  @ApiOperation({
+    summary: 'User Sign-Up',
+    description: 'Registers a new user with an email and password.',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'User successfully created.',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Invalid input data.',
+  })
   async createUser(@Body() createUserDto: CreateUserDto) {
     return await this.authService.createUser(createUserDto);
   }
 
-  // On login request -> passport (local strategy) -> authService -> queries user table and returns result
-  // @UseGuards(LocalAuthGuard)
-  // @Post('/signin')
-  // async signIn(@Body() signInUserDto: SignInUserDto) {
-  //   const user = await this.authService.signIn(signInUserDto);
-  //   const token = await this.authService.generateAccessToken(user.id);
-  //   return { user, token };
-  // }
-
-  // On login request -> passport (local strategy) -> authService -> queries user table and returns result
   @UseGuards(LocalAuthGuard)
   @Post('/signin')
+  @ApiBody({
+    type: SignInUserDto,
+    description: 'User email and password for authentication.',
+  })
+  @ApiOperation({
+    summary: 'User Sign-In',
+    description: 'Authenticates a user with email and password.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'User successfully signed in and JWT token generated.',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Invalid email or password.',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized. User not found or incorrect credentials.',
+  })
   async signIn(@Req() req: RequestWithUserInterface) {
     const { user } = req;
     const token = await this.authService.generateAccessToken(user.id);
@@ -46,31 +73,101 @@ export class AuthController {
 
   @UseGuards(AccessTokenGuard)
   @Get()
+  @ApiOperation({
+    summary: 'Get User Info',
+    description: 'Retrieves the currently logged-in user’s information.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'User information retrieved successfully.',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized. Access token is missing or invalid.',
+  })
   async getUserInfo(@Req() req: RequestWithUserInterface) {
     return await req.user;
   }
 
   @Post('/send-email-verification')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        email: {
+          type: 'string',
+          description: 'User email to send verification.',
+        },
+      },
+    },
+  })
+  @ApiOperation({
+    summary: 'Send Email Verification',
+    description:
+      'Sends an email with a verification code to the specified email address.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Verification email sent successfully.',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Invalid email address.',
+  })
   async sendEmailVerification(@Body('email') email: string) {
     return await this.authService.sendEmailVerification(email);
   }
 
   @Post('/verify-email')
-  async verifyEmail(
-    //@Body('email') email: string, @Body('code') code: string
-    @Body() emailVerificationDto: EmailVerificationDto,
-  ) {
+  @ApiBody({
+    type: EmailVerificationDto,
+    description: 'User email and the verification code sent to their inbox.',
+  })
+  @ApiOperation({
+    summary: 'Verify Email Code',
+    description: 'Verifies the email using the provided verification code.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Email verified successfully.',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Wrong or expired verification code.',
+  })
+  async verifyEmail(@Body() emailVerificationDto: EmailVerificationDto) {
     return await this.authService.verifyEmailCode(emailVerificationDto);
   }
 
-  @Get('/google')
   @UseGuards(GoogleAuthGuard)
+  @Get('/google')
+  @ApiOperation({
+    summary: 'Google Sign-In',
+    description: 'Redirects the user to Google for authentication.',
+  })
+  @ApiResponse({
+    status: 302,
+    description: 'Redirected to Google for authentication.',
+  })
   async googleSignIn() {
     return HttpStatus.OK;
   }
 
-  @Get('/google/callback')
   @UseGuards(GoogleAuthGuard)
+  @Get('/google/callback')
+  @ApiOperation({
+    summary: 'Google Sign-In Callback',
+    description:
+      'Handles the callback after Google authentication and issues a JWT token.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Google authentication successful. JWT token generated.',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized. Google authentication failed.',
+  })
   async googleSignInCallback(@Req() req: RequestWithUserInterface) {
     const { user } = req;
     const token = await this.authService.generateAccessToken(user.id);
--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -9,7 +9,13 @@ import {
 } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  ApiBody,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
 @ApiTags('products')
 @Controller('products')
@@ -17,26 +23,105 @@ export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
   @Post()
+  @ApiBody({
+    type: CreateProductDto,
+    description: 'Product details needed to create a new product.',
+  })
+  @ApiOperation({
+    summary: 'Create Product',
+    description: 'Creates a new product with the provided details.',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Product successfully created.',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Invalid input data.',
+  })
   async createProduct(@Body() createProductDto: CreateProductDto) {
     return this.productService.createProduct(createProductDto);
   }
 
   @Get()
+  @ApiOperation({
+    summary: 'Get All Products',
+    description: 'Retrieves a list of all products.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Products retrieved successfully.',
+  })
   async findAllProducts() {
     return this.productService.findAllProducts();
   }
 
   @Get(':id')
+  @ApiParam({
+    name: 'id',
+    description: 'The ID of the product to retrieve.',
+    type: String,
+  })
+  @ApiOperation({
+    summary: 'Get Product by ID',
+    description: 'Retrieves a specific product by its ID.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Product retrieved successfully.',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Product not found.',
+  })
   async findProductById(@Param('id') id: string) {
     return this.productService.findProductById(id);
   }
 
   @Delete(':id')
+  @ApiParam({
+    name: 'id',
+    description: 'The ID of the product to delete.',
+    type: String,
+  })
+  @ApiOperation({
+    summary: 'Delete Product',
+    description: 'Deletes a specific product by its ID.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Product successfully deleted.',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Product not found.',
+  })
   async removeProduct(@Param('id') id: string) {
     return this.productService.removeProduct(id);
   }
 
   @Patch(':id')
+  @ApiParam({
+    name: 'id',
+    description: 'The ID of the product to update.',
+    type: String,
+  })
+  @ApiBody({
+    type: CreateProductDto,
+    description: 'Updated product details.',
+  })
+  @ApiOperation({
+    summary: 'Update Product',
+    description: 'Updates the details of an existing product by its ID.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Product successfully updated.',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Product not found.',
+  })
   async updateProduct(
     @Param('id') id: string,
     @Body() updateProductDto: CreateProductDto,
